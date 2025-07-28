098789fdb844685e0a1d50d5f30d81307f93e151
jmanuel <manuel.gomez.quispe@gmail.com>
2025-07-28 18:22:57 -0500

    feat:proyecto togglebutton

---
+++ ToggleButton/index.js
@@ -0,0 +1,19 @@
+const toggleButton = document.querySelector("#toggle-button");
+const menu = document.querySelector("#menu");
+const hambugerIcon = document.querySelector("#hamburger-icon");
+const closeIcon = document.querySelector("#close-icon");
+
+toggleButton.addEventListener("click", () => {
+  menu.classList.toggle("hidden");
+  hambugerIcon.classList.toggle("hidden");
+  closeIcon.classList.toggle("hidden");
+});
+
+document.addEventListener("click", (event) => {
+  if (toggleButton.contains(event.target)) return;
+  if (menu.contains(event.target)) return;
+
+  menu.classList.add("hidden");
+  hambugerIcon.classList.remove("hidden");
+  closeIcon.classList.add("hidden");
+});
