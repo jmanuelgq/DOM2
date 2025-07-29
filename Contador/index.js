c29ed36bb77ae9df88cb372118d2d02343f81322
jmanuel <manuel.gomez.quispe@gmail.com>
2025-07-28 19:09:23 -0500

    feat:cont-uso de eventDelegation

--- Contador/index.js
+++ Contador/index.js
@@ -1,16 +1,36 @@
+const divContainer = document.querySelector(".counter-container");
+
 const counterValue = document.querySelector("#counter-value");
 const incrementButton = document.querySelector("#increment");
 const decrementButton = document.querySelector("#decrement");
 const resetButton = document.querySelector("#reset");
 
-incrementButton.addEventListener("click", () => {
+function incrementHandler() {
   counterValue.innerText = parseInt(counterValue.innerText) + 1;
-});
+}
 
-decrementButton.addEventListener("click", () => {
+function decrementHandler() {
   counterValue.innerText = parseInt(counterValue.innerText) - 1;
+}
+
+divContainer.addEventListener("click", function (event) {
+  if (incrementButton.contains(event.target)) {
+    incrementHandler();
+  } else if (decrementButton.contains(event.target)) {
+    decrementHandler();
+  } else if (resetButton.contains(event.target)) {
+    counterValue.innerText = 0;
+  }
 });
 
-resetButton.addEventListener("click", () => {
-  counterValue.innerText = "0";
-});
+// incrementButton.addEventListener("click", () => {
+//   counterValue.innerText = parseInt(counterValue.innerText) + 1;
+// });
+
+// decrementButton.addEventListener("click", () => {
+//   counterValue.innerText = parseInt(counterValue.innerText) - 1;
+// });
+
+// resetButton.addEventListener("click", () => {
+//   counterValue.innerText = "0";
+// });
