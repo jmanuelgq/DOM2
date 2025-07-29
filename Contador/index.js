c9af05447bca843a4e1d2971a268455bc80195ba
jmanuel <manuel.gomez.quispe@gmail.com>
2025-07-28 21:47:14 -0500

    feat:closure strategy

--- Contador/index.js
+++ Contador/index.js
@@ -5,62 +5,67 @@
 //   <button id="reset">Resetear</button>
 // </div>
 
-// const divContainer = document.querySelector(".counter-container");
+function createCounter(containerId) {
+  const container = document.querySelector(containerId);
+  container.classList.add("counter-container");
 
-// const counterValue = document.querySelector("#counter-value");
-// const incrementButton = document.querySelector("#increment");
-// const decrementButton = document.querySelector("#decrement");
-// const resetButton = document.querySelector("#reset");
+  let value = 0;
 
-//Creación de nodos
-const divContainer = document.createElement("div");
-const counterValue = document.createElement("p");
-const incrementButton = document.createElement("button");
-const decrementButton = document.createElement("button");
-const resetButton = document.createElement("button");
+  const counter = document.createElement("p");
+  const incrementButton = document.createElement("button");
+  const decrementButton = document.createElement("button");
+  const resetButton = document.createElement("button");
 
-//Agregando clases y contenido
-counterValue.textContent = "0";
-incrementButton.textContent = "Incrementar";
-decrementButton.textContent = "Decrementar";
-resetButton.textContent = "Resetear";
+  counter.textContent = value;
+  incrementButton.textContent = "Incrementar";
+  decrementButton.textContent = "Decrementar";
+  resetButton.textContent = "Resetear";
 
-divContainer.classList.add("counter-container");
-
-divContainer.append(counterValue);
-divContainer.append(incrementButton);
-divContainer.append(decrementButton);
-divContainer.append(resetButton);
-
-//Agregando funcionalidad
-function incrementHandler() {
-  counterValue.innerText = parseInt(counterValue.innerText) + 1;
-}
-
-function decrementHandler() {
-  counterValue.innerText = parseInt(counterValue.innerText) - 1;
-}
-
-divContainer.addEventListener("click", function (event) {
-  if (incrementButton.contains(event.target)) {
-    incrementHandler();
-  } else if (decrementButton.contains(event.target)) {
-    decrementHandler();
-  } else if (resetButton.contains(event.target)) {
-    counterValue.innerText = 0;
+  function incrementHandler() {
+    value += 1;
+    updateValue();
   }
-});
 
-document.body.append(divContainer);
+  function decrementHandler() {
+    value -= 1;
+    updateValue();
+  }
 
-// incrementButton.addEventListener("click", () => {
-//   counterValue.innerText = parseInt(counterValue.innerText) + 1;
-// });
+  function resetHandler() {
+    value = 0;
+    updateValue();
+  }
 
-// decrementButton.addEventListener("click", () => {
-//   counterValue.innerText = parseInt(counterValue.innerText) - 1;
-// });
+  function updateValue() {
+    counter.textContent = value;
+  }
 
-// resetButton.addEventListener("click", () => {
-//   counterValue.innerText = "0";
-// });
+  container.addEventListener("click", function (event) {
+    if (incrementButton.contains(event.target)) {
+      incrementHandler();
+    } else if (decrementButton.contains(event.target)) {
+      decrementHandler();
+    } else if (resetButton.contains(event.target)) {
+      resetHandler();
+    }
+  });
+
+  container.append(counter);
+  container.append(incrementButton);
+  container.append(decrementButton);
+  container.append(resetButton);
+
+  return {
+    increment: function () {
+      incrementHandler.click();
+    },
+    decrement: function () {
+      decrementHandler.click();
+    },
+    reset: function () {
+      resetButton.click();
+    },
+  };
+}
+
+const counter = createCounter("#counter-container");
--- ToggleButton/index.js
+++ ToggleButton/index.js
@@ -8,52 +8,34 @@
 //   <a href="">Contacto</a>
 // </nav>
 
-class ToggleMenu {
-  constructor(containerId) {
-    this.container = document.querySelector(containerId);
-    this.menuState = false;
-    this.init();
+function createToggleMenu(containerId) {
+  const container = document.querySelector(containerId);
+  let menuState = false;
+
+  const toggleButton = document.createElement("button");
+  toggleButton.textContent = "🍔";
+
+  const menu = document.createElement("nav");
+  const menuLinks = ["Inicio", "Productos", "Contacto"];
+  menuLinks.forEach((link) => {
+    const anchor = document.createElement("a");
+    anchor.href = "#";
+    anchor.textContent = link;
+    menu.append(anchor);
+  });
+
+  menu.classList.add("hidden");
+
+  function toggleMenu() {
+    menuState = !menuState;
+    menu.classList.toggle("hidden");
+    menu.classList.toggle("nav");
+    toggleButton.textContent = menuState ? "❌" : "🍔";
   }
 
-  toggleMenu() {
-    this.menuState = !this.menuState;
-    this.menu.classList.toggle("hidden");
-    this.menu.classList.toggle("nav");
-    // this.hamburgerIcon.classList.toggle("hidden");
-    // this.closeIcon.classList.toggle("hidden");
-    this.toggleButton.textContent = this.menuState ? "❌" : "🍔";
-  }
-
-  init() {
-    this.toggleButton = document.createElement("button");
-    this.toggleButton.textContent = "🍔";
-    // this.hamburgerIcon = document.createElement("span");
-    // this.closeIcon = document.createElement("span");
-
-    this.menu = document.createElement("nav");
-    const menuLinks = ["Inicio", "Productos", "Contacto"];
-    menuLinks.forEach((link) => {
-      const anchor = document.createElement("a");
-      anchor.href = "#";
-      anchor.textContent = link;
-      this.menu.append(anchor);
-    });
-
-    // this.hamburgerIcon.textContent = "🍔";
-    // this.closeIcon.textContent = "❌";
-
-    // this.closeIcon.classList.add("hidden");
-    this.menu.classList.add("hidden");
-
-    // this.toggleButton.append(this.hamburgerIcon);
-    // this.toggleButton.append(this.closeIcon);
-
-    // this.toggleButton.addEventListener("click", this.toggleMenu.bind(this));
-    this.toggleButton.addEventListener("click", () => this.toggleMenu());
-
-    this.container.append(this.toggleButton);
-    this.container.append(this.menu);
-  }
+  toggleButton.addEventListener("click", toggleMenu);
+  container.append(toggleButton);
+  container.append(menu);
 }
 
-new ToggleMenu("#menu-container");
+const toggleMenu = createToggleMenu("#menu-container");
